252dd70f95c7b46e5e04a586cc0975609b89048a
Phi Tran <phitran@Phis-MBP.attlocal.net>
2019-01-16 22:26:43 -0600

    server is up and routes are working correctly

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
  
 // parse some custom thing into a Buffer
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
- 
+
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
+
+require(".//app/routing/html-routes")(app); //requiring file 
 
 app.listen(PORT, function () {
     console.log("Server listneing on: http://localhost: " + PORT);
