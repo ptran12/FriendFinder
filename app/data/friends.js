fea805dd1735aac5921769b67d909aa9f123ed96
Phi Tran <phitran@Phis-MBP.attlocal.net>
2019-01-17 23:14:21 -0600

    friends data

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+let friends = [
+    {
+        "name": 'Lebron James',
+        "photo": '',
+        "scores": [5, 5, 5, 5, 4, 4, 4, 3, 3, 3]
+    },
+    {
+        "name": "Kyrie Irving",
+        "photo": "",
+        "scores": [5, 5, 5, 4, 3, 2, 3, 2, 2, 2]
+    },
+    {
+        "name": "DeMar Derozan",
+        "photo": "",
+        "scores": [4, 4, 3, 3, 3, 1, 5, 2, 3, 4]
+    },
+    {
+        "name": "Anthony Davis",
+        "photo": "",
+        "scores": [2, 2, 2, 1, 4, 1, 5, 5, 4, 3]
+    },
+    {
+        "name": "James Harden",
+        "photo": "",
+        "scores": [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+    },
+    {
+        "name": "Stephen Curry",
+        "photo": "",
+        "scores": [1, 3, 2, 4, 5, 1, 2, 3, 4, 5]
+    },
+    {
+        "name": "Kevin Durant",
+        "photo": "",
+        "scores": [3, 4, 5, 2, 1, 4, 4, 1, 1, 2]
+    },
+    {
+        "name": "Kawhi Leonard",
+        "photo": "",
+        "scores": [5, 5, 3, 1, 3, 2, 3, 2, 1, 5]
+    },
+    {
+        "name": "Klay Thompson",
+        "photo": "",
+        "scores": [1, 1, 2, 4, 1, 4, 3, 5, 1, 3]
+    },
+    {
+        "name": "Lamarcus Aldridge",
+        "photo": "",
+        "scores": [2, 2, 3, 1, 4, 3, 4, 3, 5, 4]
+    },
+
+]
+
+module.exports = friends;
