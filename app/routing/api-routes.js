5a508aa2d9c9523b0f47229cdc36401dd29d15eb
Phi Tran <phitran@Phis-MBP.attlocal.net>
2019-01-18 21:33:17 -0600

    server js, footer, api routes, survery

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,7 +1,46 @@
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function (app) {
     app.get("/api/friends", function(req, res){
         res.json(friends);
     });
+
+    app.post("/api/friends", function(req, res){
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+        };
+        console.log(req.body);
+
+        var userData = req.body
+        var userScores = userData.scores;
+
+        console.log(userScores);
+
+        var totalDifference = 0;
+
+        for(var i = 0; i < friends.length; i++){
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            for(var t = 0; t < friends[i].scores[t]; t++){
+                totalDifference += Math.abs(parseInt(userScores[t]) - parseInt(friends[i].scores[t]));
+                if(totalDifference <= bestMatch.friendDifference) {
+
+
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+        friends.push(userData);
+
+        res.json(bestMatch);
+    });
+
+   
 }
+
+
--- server.js
+++ server.js
@@ -5,12 +5,7 @@ var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080
 
 
-// create application/json parser
-var jsonParser = bodyParser.json()
- 
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
- 
+app.use(bodyParser.urlencoded({ extended: true }));
  
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
@@ -25,5 +20,5 @@ require("./app/routing/api-routes")(app) //api route
 require(".//app/routing/html-routes")(app); //requiring file 
 
 app.listen(PORT, function () {
-    console.log("Server listneing on: http://localhost: " + PORT);
+    console.log("Server listneing on: http://localhost:"+PORT);
 });
